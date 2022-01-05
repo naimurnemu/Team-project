d27dec0a2fa5b1fe54861d7b654de2df76e57473
naimurnemu <naimurrahaman82@gmail.com>
2022-01-05 18:34:57 +0600

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Registration/Login/Login";
 
 function App() {
     return (
         <div className="App">
+            <BrowserRouter>
+            </BrowserRouter>
             <Login />
         </div>
     );
---
+++ src/pages/Shared/NavigationHeader/NavigationHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavigationHeader = () => {
+    return (
+        <div>
+            <h2>This is navigation header</h2>
+        </div>
+    );
+};
+
+export default NavigationHeader;
