1b5a0f27ef4b4d54025ca2f1fddb262005b16e71
naimurnemu <naimurrahaman82@gmail.com>
2022-01-09 12:15:37 +0600

    changes

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -3,26 +3,18 @@ import NavigationHeader from "../../Shared/NavigationHeader/NavigationHeader";
 import About from "../About/About";
 import Banner from "../Banner/Banner";
 import Contact from "../Contact/Contact";
-import Controls from "../Controls/Controls";
 import CulturalProgram from "../CulturalProgram/CulturalProgram";
 import Footer from "../Footer/Footer";
 
-
 const Home = () => {
-
-   
-    
     return (
         <div>
             <NavigationHeader />
-            <Banner></Banner>
-            
-            <Controls></Controls>
+            <Banner />
             <CulturalProgram></CulturalProgram>
             <About></About>
             <Contact></Contact>
-            <Footer/>
-            
+            <Footer />
         </div>
     );
 };
