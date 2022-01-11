86ad42441a57b30627c24bfd37445afa1d3054db
naimurnemu <naimurrahaman82@gmail.com>
2022-01-11 14:11:18 +0600

    done

--- src/pages/Registration/ProtectedRoute/AdminRoute.js
+++ src/pages/Registration/ProtectedRoute/AdminRoute.js
@@ -18,7 +18,7 @@ const AdminRoute = ({ children }) => {
     } else if (user.email) {
         return children;
     } else {
-        return <Navigate to="/addUser" state={{ from: location }} />;
+        return <Navigate to="/login" state={{ from: location }} />;
     }
 };
 
